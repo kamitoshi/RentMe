3480b3760e5f84576c231385547bd77a56f9b56f
kamitoshi <kroo.aq.74@gmail.com>
2020-06-21 22:36:11 +0900

    do setting worker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get '/about', to: "home#about", as: "about"
+  devise_for :workers, controllers:{
+    sessions: "workers/sessions",
+    passwords: "workers/passwords",
+    registrations: "workers/registrations"
+  }
+  devise_scope :worker do
+    get 'confirm_email', to: 'workers/registrations#confirm_email'
+  end
+  resources :workers, only:[:index, :show, :edit, :update, :destroy] do
+    member do
+      get :image_edit
+    end
+    collection do
+      get :delete
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
