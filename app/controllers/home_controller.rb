12e084c90ea502f2630aef4a4d81e9c301866be3
kamitoshi <kroo.aq.74@gmail.com>
2020-06-29 12:32:30 +0900

    modify employer pages design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,20 @@
 class HomeController < ApplicationController
   skip_before_action :login?
+  before_action :signed_in_user?
 
   def top
   end
 
   def about
   end
+
+  private
+  def signed_in_user?
+    if worker_signed_in?
+      redirect_to workers_path
+    elsif employer_signed_in?
+      redirect_to employers_path
+    end
+  end
+  
 end
