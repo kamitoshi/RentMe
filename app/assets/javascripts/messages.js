6398849c05e35779cf908518a7eba0a5d6a6a63d
kamitoshi <kroo.aq.74@gmail.com>
2020-07-08 11:23:30 +0900

    setting scrollBottom for message

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+window.onload = function scrollBottom() {
+  var target = document.getElementById("scroll-inner");
+  target.scrollIntoView(false);
+};
